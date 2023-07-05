c189a9ad2d1ebfc26d6b993b6b4c3df78b81e33a
AnatoliiCubex <anatolii.pyroh.cubex@gmail.com>
2023-07-05 13:59:19 +0300

    updated form styles

--- src/views/CreateTournamentPage/CreateTournamentPage.tsx
+++ src/views/CreateTournamentPage/CreateTournamentPage.tsx
@@ -1,26 +1,58 @@
-import { TextField, Button, Box } from "@mui/material";
+import { TextField, Button, Box, Typography } from "@mui/material";
 
 import styles from "./CreateTournamentPage.module.scss";
 
 export const CreateTournamentPageComponent = () => {
   return (
-    <Box component='form' onSubmit={() => alert(123)} className={styles.form}>
-      <TextField size='small' label='Title'></TextField>
+    <Box className={styles.formContainer}>
+      <Box className={styles.formImage}>
+        <Typography
+          fontSize={"4rem"}
+          sx={{ fontWeight: "bold", color: "white" }}
+          component={"h1"}
+        >
+          Hamilton.
+        </Typography>
+        <Typography
+          fontSize={"1.5rem"}
+          sx={{ fontWeight: "bold", color: "white" }}
+        >
+          Create tournaments for you wish.
+        </Typography>
+        <Typography fontSize={"1rem"} sx={{ color: "white" }}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id tenetur
+          voluptates molestiae optio quas voluptate cum quisquam ea, libero
+          corrupti reprehenderit repellat fugit rem expedita eveniet, dicta
+          magni dolorum ratione!
+        </Typography>
+      </Box>
 
-      <TextField size='small' label='Description'></TextField>
-      <TextField size='small' label='Number of players'></TextField>
+      <Box component='form' onSubmit={() => alert(123)} className={styles.form}>
+        <Typography fontSize={"2rem"} sx={{ fontWeight: "bold" }}>
+          Create tournament
+        </Typography>
+        <TextField size='small' label='Title'></TextField>
+        <TextField size='small' label='Description'></TextField>
+        <TextField size='small' label='Number of players'></TextField>
+        <TextField size='small' id='entry-fee' label='Entry fee' />
+        <TextField size='small' label='Prize distribution'></TextField>
 
-      <TextField size='small' id='entry-fee' label='Entry fee' />
+        <Button
+          type='submit'
+          variant='contained'
+          sx={{
+            width: "fit-content",
+            alignSelf: "center",
+            backgroundColor: "hsl(337deg 100% 43%)",
 
-      <TextField size='small' label='Prize distribution'></TextField>
-
-      <Button
-        type='submit'
-        variant='contained'
-        sx={{ width: "fit-content", alignSelf: "center" }}
-      >
-        Submit
-      </Button>
+            "&:hover": {
+              backgroundColor: "hsl(329deg 100% 36%)",
+            },
+          }}
+        >
+          Submit
+        </Button>
+      </Box>
     </Box>
   );
 };
